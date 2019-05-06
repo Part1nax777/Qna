88db0d0c856c9d4bd290324b213d4752f1427d37
Part1nax777 <serewka-ru@yandex.ru>
2019-05-06 21:05:51 +0300

    from screencast

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,48 @@
+class QuestionsController < ApplicationController
+  #before_action :load_question, only: %i[show edit update destroy]
+  def index
+    @questions = Question.all
+  end
+
+  def show; end
+
+  def new
+  end
+
+  def edit; end
+
+  def create
+    @question = Question.new(questions_params)
+
+    if @question.save
+      redirect_to(@question)
+    else
+      render :new
+    end
+  end
+
+  def update
+    if question.update(questions_params)
+      redirect_to(@question)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    question.destroy
+    redirect_to questions_path
+  end
+
+  private
+
+  def question
+    @question ||= params[:id] ? Question.find(params[:id]) : Question.new
+  end
+
+  helper_method :question
+
+  def questions_params
+    params.require(:question).permit(:title, :body)
+  end
+end
