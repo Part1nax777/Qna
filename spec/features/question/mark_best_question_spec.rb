8f3b1c4dde0ffaeeec7b0c9192077818b8cc62ba
Part1nax777 <serewka-ru@yandex.ru>
2019-07-01 15:59:24 +0300

    add feature test for best answer is first

--- spec/features/question/mark_best_question_spec.rb
+++ spec/features/question/mark_best_question_spec.rb
@@ -10,9 +10,7 @@ feature 'User can mark best answer', %q{
   given!(:another_user) { create(:user) }
   given!(:question) { create(:question, user: user) }
   given!(:another_question) { create(:question, user: another_user) }
-  given!(:answer) { create(:answer, question: question, user: user) }
-  given!(:answer2) { create(:answer, question: question, user: user) }
-
+  given!(:answers) { create_list(:answer, 3, question: question, user: user) }
 
   describe 'Authenticated user', js: true do
 
@@ -24,13 +22,12 @@ feature 'User can mark best answer', %q{
       visit question_path(question)
 
       first('.mark-as-best').click
-      first('.mark-as-best', {}, &:visible?).click
 
       expect(page).to have_css('.best', count: 1)
     end
 
     scenario 'Author of question can mark another best question' do
-      answer.best = true
+      answers.last.best = true
       visit question_path(question)
 
       first('.mark-as-best').click
@@ -43,6 +40,18 @@ feature 'User can mark best answer', %q{
       visit question_path(another_question)
 
       expect(page).to_not have_link 'Best answer'
+    end
+
+    scenario 'Best answer is a first' do
+      visit question_path(question)
+
+      best_answer = answers.last
+      within("#answer-#{best_answer.id}") { click_on 'Best answer' }
+      first_answer = find('.answers').first(:element)
+
+      within first_answer do
+        expect(page).to have_content best_answer.body
+      end
     end
   end
 
