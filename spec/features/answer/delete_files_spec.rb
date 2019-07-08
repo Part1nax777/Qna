21851012538a2893da93f5d61d9584175577335b
Part1nax777 <serewka-ru@yandex.ru>
2019-07-08 11:11:35 +0300

    author of answer can delete files

--- spec/features/answer/delete_files_spec.rb
+++ spec/features/answer/delete_files_spec.rb
@@ -1,29 +1,25 @@
 require 'rails_helper'
 
-feature 'Author can delete attached files', %q(
-  The author of the question
+feature 'Author can delete attached files', %q{
+  The author of the answer
   Can delete any files
-  Attached to the question
-) do
+  Attached to the answer
+} do
 
   given(:user) { create(:user) }
   given(:another_user) { create(:user) }
   given(:question) { create(:question, user: user) }
 
-  describe 'Try delete attached file to the question', js: true do
+  describe 'Try delete attached file to the answer', js: true do
     background do
       sign_in(user)
       visit question_path(question)
-      click_on 'Edit question'
-      within ".question" do
-        fill_in 'Title', with: 'question title'
-        fill_in 'Body', with: 'question body'
-        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb"]
-        click_on 'Save'
-      end
+      fill_in 'Body', with: 'answer body'
+      attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb"]
+      click_on 'Create answer'
     end
 
-    scenario 'Author of question' do
+    scenario 'Author of answer' do
       click_on 'Delete file'
 
       expect(page).to_not have_content 'rails_helper.rb'
@@ -33,7 +29,7 @@ feature 'Author can delete attached files', %q(
       click_on 'Logout'
       visit question_path(question)
 
-      expect(page).to_not have_content'Delete file'
+      expect(page).to_not have_link 'Delete file'
     end
 
     scenario 'Another user' do
@@ -41,7 +37,7 @@ feature 'Author can delete attached files', %q(
       sign_in(another_user)
       visit question_path(question)
 
-      expect(page).to_not have_content'Delete file'
+      expect(page).to_not have_link 'Delete file'
     end
   end
 end
